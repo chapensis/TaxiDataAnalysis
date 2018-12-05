f8e4c28eb6822327aa9aff4b42440bea74393664
yangchang <635958533@qq.com>
2018-12-05 22:35:51 +0800

    笔记本电脑最后一次上传

--- TaxiDataAnalysis/src/main/java/com/yangchang/TaxiDataAnalysis/config/WebMvcConfig.java
+++ TaxiDataAnalysis/src/main/java/com/yangchang/TaxiDataAnalysis/config/WebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.yangchang.TaxiDataAnalysis.config;
 
 import com.yangchang.TaxiDataAnalysis.controller.interceptor.ControllerInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ControllerInterceptor()).addPathPatterns("/*/**");
--- TaxiDataAnalysis/src/main/java/com/yangchang/TaxiDataAnalysis/controller/interceptor/ControllerInterceptor.java
+++ TaxiDataAnalysis/src/main/java/com/yangchang/TaxiDataAnalysis/controller/interceptor/ControllerInterceptor.java
@@ -3,6 +3,7 @@ package com.yangchang.TaxiDataAnalysis.controller.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,20 @@ public class ControllerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("ControllerInterceptor preHandle ...");
+        try {
+            // 1、设置IP地址和端口
+            Jedis jedis = new Jedis("127.0.0.1", 6379);
+
+            // 2、保存数据
+            jedis.incr("visits");
+            log.info("当前访问次数：" + jedis.get("visits"));
+
+            // 4、关闭连接
+            jedis.close();
+        } catch (Exception e) {
+            log.error("jedis operate error:", e);
+        }
+
         return true;
     }
 
